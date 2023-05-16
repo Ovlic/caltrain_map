a9aac3cd61373529dc836f4ff073ca33acffe4c3
Ovlic <74332950+Ovlic@users.noreply.github.com>
2023-05-16 10:06:49 -0700

    Add comments to data

--- data.py
+++ data.py
@@ -4,6 +4,11 @@ import requests
 base_url = "https://api.511.org/transit/"
 
 class CaltrainData:
+    """
+    Caltrain Data class for getting data from the 511 transit api.
+    # Parameters:
+    api_key (str): The api key for the 511 transit api.
+    """
     def __init__(self, api_key):
         self.api_key = api_key
         self.session = requests.Session()
@@ -11,10 +16,11 @@ class CaltrainData:
         self.session.params["agency"] = "CT"
 
     def get_vehicle_locations(self):
+        """Get live vehicle locations for Caltrain.
+        # Returns:
+        requests.Response: The response from the api.
+        """
         url = base_url + "VehicleMonitoring"
         response = self.session.get(url)
         # print(response.text)
         return response
-        
-
-    
