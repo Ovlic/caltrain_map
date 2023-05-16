0ed40978354205a8fd823b106009a5fd06ea4f30
Ovlic <74332950+Ovlic@users.noreply.github.com>
2023-05-16 15:21:11 -0700

    Add docstrings and types to functions

--- data.py
+++ data.py
@@ -7,20 +7,17 @@ class CaltrainData:
     """
     Caltrain Data class for getting data from the 511 transit api.
     # Parameters:
-    api_key (str): The api key for the 511 transit api.
+    api_key: :class:`str`
+        The api key for the 511 transit api.
     """
-    def __init__(self, api_key):
-        self.api_key = api_key
+    def __init__(self, api_key:str):
         self.session = requests.Session()
-        self.session.params["api_key"] = self.api_key
+
+        self.session.params["api_key"] = api_key
         self.session.params["agency"] = "CT"
 
-    def get_vehicle_locations(self):
-        """Get live vehicle locations for Caltrain.
-        # Returns:
-        requests.Response: The response from the api.
-        """
+    def get_vehicle_locations(self) -> requests.Response:
+        """Get live vehicle locations for Caltrain."""
         url = base_url + "VehicleMonitoring"
         response = self.session.get(url)
-        # print(response.text)
         return response
