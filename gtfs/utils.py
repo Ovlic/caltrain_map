734b52d596fb4aaa9a4eab5a8e630ceb11ae3630
Ovlic <74332950+Ovlic@users.noreply.github.com>
2023-05-17 08:13:52 -0700

    Add type checking and docstrings

--- gtfs/utils.py
+++ gtfs/utils.py
@@ -1,8 +1,16 @@
+"""
+This module contains utility functions for the GTFS package.
+"""
 
 from datetime import datetime
 from pytz import timezone
 
-def toDateTime(str_time):
+def toDateTime(str_time) -> datetime:
+    """Converts a string time to a datetime object.
+    # Parameters:
+    str_time: :class:`str`
+        The string time to convert.
+    """
     str_time = str_time.replace("T", "-").replace("Z", "")
     date = datetime.strptime(str_time, "%Y-%m-%d-%H:%M:%S").replace(tzinfo=timezone('UTC'))
     date = date.astimezone(timezone('US/Pacific'))
