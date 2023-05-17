76f7d2f98fbf68ed97e90894c6104b0203911412
Ovlic <74332950+Ovlic@users.noreply.github.com>
2023-05-17 08:05:47 -0700

    Add type hinting and docstrings to functions

--- gtfs/__init__.py
+++ gtfs/__init__.py
@@ -4,14 +4,16 @@ from .stop import Stop
 from .realtime_vehicles import VehicleActivity
     
 
-def convert_shape_gtfs_to_shape(shape_gtfs):
+def convert_shape_gtfs_to_shape(shape_gtfs: str) -> list(Shape):
+    """Converts a string shape GTFS to a Shape object."""
     shape_gtfs = shape_gtfs.split("\n")[1:]
-    shape_gtfs = [Shape(x.split(",")) for x in shape_gtfs]
-    return shape_gtfs
+    l_shape_gtfs = [Shape(x.split(",")) for x in shape_gtfs]
+    return l_shape_gtfs
 
-def convert_stop_gtfs_to_stop(stop_gtfs):
+def convert_stop_gtfs_to_stop(stop_gtfs) -> list(Stop):
+    """Converts a string stop GTFS to a Stop object."""
     stop_gtfs = stop_gtfs.split("\n")[1:]
     for item in stop_gtfs:
         item = item.replace('"', '')
-    stop_gtfs = [Stop(x.split(",")) for x in stop_gtfs]
-    return stop_gtfs
+    l_stop_gtfs = [Stop(x.split(",")) for x in stop_gtfs]
+    return l_stop_gtfs
