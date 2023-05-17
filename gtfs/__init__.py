2e5d8eda8d8b094adf9b3dfbab37ee10d02ad407
Ovlic <74332950+Ovlic@users.noreply.github.com>
2023-05-17 10:15:11 -0700

    Fix TypeError

--- gtfs/__init__.py
+++ gtfs/__init__.py
@@ -7,13 +7,13 @@ from .stop import Stop
 from .realtime_vehicles import VehicleActivity
     
 
-def convert_shape_gtfs_to_shape(shape_gtfs: str) -> list(Shape):
+def convert_shape_gtfs_to_shape(shape_gtfs: str) -> list:
     """Converts a string shape GTFS to a Shape object."""
     shape_gtfs = shape_gtfs.split("\n")[1:]
     l_shape_gtfs = [Shape(x.split(",")) for x in shape_gtfs]
     return l_shape_gtfs
 
-def convert_stop_gtfs_to_stop(stop_gtfs) -> list(Stop):
+def convert_stop_gtfs_to_stop(stop_gtfs) -> list:
     """Converts a string stop GTFS to a Stop object."""
     stop_gtfs = stop_gtfs.split("\n")[1:]
     for item in stop_gtfs:
